1d82dd0e812a9659ff25e70d8643f329198f6144
mbj36 <mohitbajo36@gmail.com>
2018-08-31 21:07:17 +0530

    docs:working on demo

--- src/App.vue
+++ src/App.vue
@@ -2,49 +2,57 @@
     <div id="app">
         <span v-if="checkSide === 'right'">
             <Menu right>
-                <a href="">
+                <a href="#">
                     <i class="fa fa-fw fa-star-o"></i>
+                    <span>Favourites</span>
+                </a>
+                <a href="#">
+                    <i class="fa fa-fw fa-bell-o"></i>
+                    <span>Alerts</span>
+                </a>
+                <a href="#">
+                    <i class="fa fa-fw fa-envelope-o"></i>
                     <span>Messages</span>
                 </a>
-                <a href="">
-                    <i class="fa fa-fw fa-star-o"></i>
-                    <span>Messages</span>
+                <a href="#">
+                    <i class="fa fa-fw fa-comment-o"></i>
+                    <span>Comments</span>
                 </a>
-                <a href="">
-                    <i class="fa fa-fw fa-star-o"></i>
-                    <span>Messages</span>
+                <a href="#">
+                    <i class="fa fa-fw fa-bar-chart-o"></i>
+                    <span>Analytics</span>
                 </a>
-                <a href="">
-                    <i class="fa fa-fw fa-star-o"></i>
-                    <span>Messages</span>
-                </a>
-                <a href="">
-                    <i class="fa fa-fw fa-star-o"></i>
-                    <span>Messages</span>
+                <a href="#">
+                    <i class="fa fa-fw fa-newspaper-o"></i>
+                    <span>Reading List</span>
                 </a>
             </Menu>
         </span>
         <span v-else>
             <Menu>
-                <a href="">
+                <a href="#">
                     <i class="fa fa-fw fa-star-o"></i>
+                    <span>Favourites</span>
+                </a>
+                <a href="#">
+                    <i class="fa fa-fw fa-bell-o"></i>
+                    <span>Alerts</span>
+                </a>
+                <a href="#">
+                    <i class="fa fa-fw fa-envelope-o"></i>
                     <span>Messages</span>
                 </a>
-                <a href="">
-                    <i class="fa fa-fw fa-star-o"></i>
-                    <span>Messages</span>
+                <a href="#">
+                    <i class="fa fa-fw fa-comment-o"></i>
+                    <span>Comments</span>
                 </a>
-                <a href="">
-                    <i class="fa fa-fw fa-star-o"></i>
-                    <span>Messages</span>
+                <a href="#">
+                    <i class="fa fa-fw fa-bar-chart-o"></i>
+                    <span>Analytics</span>
                 </a>
-                <a href="">
-                    <i class="fa fa-fw fa-star-o"></i>
-                    <span>Messages</span>
-                </a>
-                <a href="">
-                    <i class="fa fa-fw fa-star-o"></i>
-                    <span>Messages</span>
+                <a href="#">
+                    <i class="fa fa-fw fa-newspaper-o"></i>
+                    <span>Reading</span>
                 </a>
             </Menu>
         </span>
