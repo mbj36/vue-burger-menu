4b2b540f14d7f8222d76d108591b30d6699bae56
Mouadh Hsoumi <mouadh@hsoumi.com>
2019-02-06 23:22:17 +0100

    [removed] vue-burger-menu : debug file removed

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import App2 from './App2.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App2)
+  render: h => h(App)
 }).$mount('#app')
